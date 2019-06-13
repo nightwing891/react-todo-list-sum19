147255c31df9778caa68ae1f6d684cc235e91078
Henry Doan <henrydoan96@gmail.com>
2019-06-12 20:50:57 -0600

    end of day 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
+import List from './components/todos/List';
+import TodoForm from './components/todos/TodoForm';
 
-function App() {
-  return (
-    <div>
-     
-    </div>
-  );
+class App extends Component {
+  state = { 
+    todos: [
+      { id: 1, name: 'Learn Rails', complete: true },
+      { id: 2, name: 'Learn React', complete: false },
+      { id: 3, name: 'Learn Router', complete: false }
+    ] 
+  }
+  
+  render() {
+    const { todos } = this.state
+    return (
+      <div>
+        <TodoForm />
+        <List name='Todo List' items={todos}/>
+      </div>
+    );
+  }
 }
 
 export default App;
